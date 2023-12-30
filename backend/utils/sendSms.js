48957fc4d88eef71b7615df49815a85b522612b1
mrm15 <mrm.learn@gmail.com>
2023-12-30 10:54:49 +0330

    Update sendSms.js

--- backend/utils/sendSms.js
+++ backend/utils/sendSms.js
@@ -1,9 +1,11 @@
 const axios = require('axios')
 const sendSms = async (text, destinationNumber) => {
-  const url = `https://niksms.com/fa/publicapi/groupsms?username=09126970541&password=Endj174622endj&numbers=${destinationNumber}&sendernumber=50004307&message=${text}`
+
+  return  true
+  const url = `https://niksms.com/fa/publicapi/groupsms?username=09126970541&password=Endj174622endj&numbers=${destinationNumber}&sendernumber=50002660&message=${text}`
   const res = await axios.get(url);
   console.log(res.data)
-  debugger
+   
   if (res?.data?.Status === 1) {
     // thats ok
     return true
